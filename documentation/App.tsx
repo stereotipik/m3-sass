1ec9b656655eb549237ec47a510599740d1dc243
Michel Montejuado <michel.montejuado@gmail.com>
2022-06-07 07:09:34 +0200

    Initialization

---
+++ documentation/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <header>
+        <img src="/logo.svg" alt="logo" width={128} />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </>
+  );
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,5 @@
+# @stereotipik • m3-sass
+
+My attempt at an implementation of the Material Design 3 specifications.
+
+**Project in very early stages, will probably get rebranded or abandonned at some point.**
